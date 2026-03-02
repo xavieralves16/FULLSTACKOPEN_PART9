81efd9753fb631f2eacf100b69f08e1d6798e44f
Xavier <xavieralves162@gmail.com>
2026-03-02 16:34:28 +0000

    Exc 9.1

---
+++ bmiCalculator.ts
@@ -0,0 +1,39 @@
+interface BMIValues {
+  height: number; 
+  weight: number; 
+}
+
+const parseArguments = (args: string[]): BMIValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  const height = Number(args[2]);
+  const weight = Number(args[3]);
+
+  if (isNaN(height) || isNaN(weight)) {
+    throw new Error('Provided values were not numbers!');
+  }
+
+  return { height, weight };
+};
+
+const calculateBmi = (height: number, weight: number): string => {
+  const bmi = weight / (height * height);
+
+  if (bmi < 18.5) return 'Underweight';
+  if (bmi >= 18.5 && bmi < 25) return 'Normal weight';
+  if (bmi >= 25 && bmi < 30) return 'Overweight';
+  return 'Obese';
+};
+
+try {
+  const { height, weight } = parseArguments(process.argv);
+  const result = calculateBmi(height, weight);
+  console.log(`${result}`);
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
