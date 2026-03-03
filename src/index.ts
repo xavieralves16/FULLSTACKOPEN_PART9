263f731d5fa88c076caccf07e377594a47a6fd65
Xavier <xavieralves162@gmail.com>
2026-03-03 14:50:47 +0000

    Exc 9.7

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { calculateBmi } from '../bmiCalculator';
+import { calculateExercises } from '../exerciseCalculator';
 
 const app = express();
 const PORT = 3003;
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
@@ -29,6 +31,35 @@ app.get('/bmi', (req, res) => {
     height: heightNum,
     bmi
   });
+});
+
+app.post('/exercises', (req, res) => {
+  const body = req.body as unknown;
+
+  if (!body || typeof body !== 'object') {
+    return res.status(400).json({ error: 'parameters missing' });
+  }
+
+  if (!('daily_exercises' in body) || !('target' in body)) {
+    return res.status(400).json({ error: 'parameters missing' });
+  }
+
+  const { daily_exercises, target } = body as {
+    daily_exercises: unknown;
+    target: unknown;
+  };
+
+  if (
+    !Array.isArray(daily_exercises) ||
+    typeof target !== 'number' ||
+    !daily_exercises.every(e => typeof e === 'number')
+  ) {
+    return res.status(400).json({ error: 'malformatted parameters' });
+  }
+
+  const result = calculateExercises(daily_exercises, target);
+
+  return res.json(result);
 });
 
 app.listen(PORT, () => {
