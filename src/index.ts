6c499662cd7b310cc7cff56a5fbc865ea5ce7723
Xavier <xavieralves162@gmail.com>
2026-03-03 13:55:40 +0000

    Exc 9.4

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/hello', (_req, res) => {
+  res.send('Hello Full Stack!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
