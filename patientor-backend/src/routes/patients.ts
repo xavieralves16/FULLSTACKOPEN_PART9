fdf77ddcc8a58061885ce96c272aa94c119ce671
Xavier <xavieralves162@gmail.com>
2026-03-04 12:01:07 +0000

    Exc 9.12

--- patientor-backend/src/routes/patients.ts
+++ patientor-backend/src/routes/patients.ts
@@ -1,11 +1,20 @@
-import express, { Response } from 'express';
+import express, { Request, Response } from 'express';
 import patientService from '../services/patientsService';
-import { NonSensitivePatient } from '../types';
+import { NonSensitivePatient , Patient} from '../types';
 
 const router = express.Router();
 
 router.get('/', (_req, res: Response<NonSensitivePatient[]>) => {
   res.json(patientService.getNonSensitivePatients());
+});
+
+router.post('/', (req: Request, res: Response<Patient | { error: string }>) => {
+  try {
+    const newPatient = patientService.addPatient(req.body);
+    res.json(newPatient);
+  } catch (e) {
+    res.status(400).send({ error: 'Invalid patient data' });
+  }
 });
 
 export default router;
--- patientor-backend/src/services/patientsService.ts
+++ patientor-backend/src/services/patientsService.ts
@@ -1,5 +1,6 @@
 import patients from '../data/patients';
 import { Patient, NonSensitivePatient } from '../types';
+import { v1 as uuid } from 'uuid';
 
 const getPatients = (): Patient[] => {
   return patients;
@@ -9,7 +10,15 @@ const getNonSensitivePatients = (): NonSensitivePatient[] => {
   return patients.map(({ ssn, ...rest }) => rest);
 };
 
+const addPatient = (patient: Omit<Patient, 'id'>): Patient => {
+  const newPatient = { id: uuid(), ...patient };
+  patients.push(newPatient);
+  return newPatient;
+};
+
+
 export default {
   getPatients,
-  getNonSensitivePatients
+  getNonSensitivePatients,
+  addPatient
 };
