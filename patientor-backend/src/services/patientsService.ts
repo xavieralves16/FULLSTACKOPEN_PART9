ed3c1a707a085f350e47530232612e6909ad8aed
Xavier <xavieralves162@gmail.com>
2026-03-04 12:14:21 +0000

    Exc 9.13

--- patientor-backend/src/services/patientsService.ts
+++ patientor-backend/src/services/patientsService.ts
@@ -1,6 +1,11 @@
-import patients from '../data/patients';
-import { Patient, NonSensitivePatient } from '../types';
+import patientsData from '../data/patients';
+import { Patient, NonSensitivePatient , isPatientData, Gender} from '../types';
 import { v1 as uuid } from 'uuid';
+
+let patients: Patient[] = patientsData.map(p => ({
+  ...p,
+  gender: p.gender as Gender 
+}));
 
 const getPatients = (): Patient[] => {
   return patients;
@@ -10,15 +15,23 @@ const getNonSensitivePatients = (): NonSensitivePatient[] => {
   return patients.map(({ ssn, ...rest }) => rest);
 };
 
-const addPatient = (patient: Omit<Patient, 'id'>): Patient => {
-  const newPatient = { id: uuid(), ...patient };
-  patients.push(newPatient);
+const addPatient = (patientData: any): Patient => {
+  if (!isPatientData(patientData)) {
+    throw new Error('Invalid patient data');
+  }
+
+  const newPatient: Patient = {
+    id: uuid(),
+    ...patientData
+  };
+
+  patients.push(newPatient); 
   return newPatient;
 };
-
 
 export default {
   getPatients,
   getNonSensitivePatients,
-  addPatient
+  addPatient,
+  patients
 };
--- patientor-backend/src/types.ts
+++ patientor-backend/src/types.ts
@@ -4,12 +4,36 @@ export interface Diagnosis {
   latin?: string; 
 }
 
+export enum Gender {
+  Male = 'male',
+  Female = 'female',
+  Other = 'other'
+}
+
+const isString = (text: unknown): text is string => {
+  return typeof text === 'string' || text instanceof String;
+};
+
+const isGender = (param: any): param is Gender => {
+  return Object.values(Gender).includes(param);
+};
+
+export const isPatientData = (obj: any): obj is Omit<Patient, 'id'> => {
+  return obj &&
+    isString(obj.name) &&
+    isString(obj.dateOfBirth) &&
+    isString(obj.ssn) &&
+    isGender(obj.gender) &&
+    isString(obj.occupation);
+};
+
+
 export interface Patient {
   id: string;
   name: string;
   dateOfBirth: string;
   ssn: string;
-  gender: string; 
+  gender: Gender; 
   occupation: string;
 }
 
