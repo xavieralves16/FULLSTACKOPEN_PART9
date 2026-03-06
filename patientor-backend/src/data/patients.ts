f79607dbe0d62857f2f2043244032eea884d9482
Xavier <xavieralves162@gmail.com>
2026-03-06 10:02:09 +0000

    Exc 9.23

--- patientor-backend/src/data/patients.ts
+++ patientor-backend/src/data/patients.ts
@@ -8,7 +8,22 @@ const data: Patient[] = [
     ssn: "090786-122X",
     gender: Gender.Male,
     occupation: "New York City cop",
-    entries: []
+    entries: [
+      {
+        id: "d811e46d-70b3-4d90-b090-4535c7cf8fb1",
+        date: "2015-01-02",
+        type: "Hospital",
+        specialist: "MD House",
+        description: "Healing time appr. 2 weeks. patient doesn't remember how he got the injury.",
+        diagnosisCodes: ["S62.5"],
+        discharge: {
+          date: "2015-01-16",
+          criteria: "Thumb has healed."
+        }
+      }
+    ]
   }
 ];
+
+
 export default data;
--- patientor-backend/src/types.ts
+++ patientor-backend/src/types.ts
@@ -27,9 +27,6 @@ export const isPatientData = (obj: any): obj is Omit<Patient, 'id'> => {
     isString(obj.occupation);
 };
 
-
-export interface Entry {}
-
 export interface Patient {
   id: string;
   name: string;
@@ -41,3 +38,50 @@ export interface Patient {
 }
 
 export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
+
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+  Healthy = 0,
+  LowRisk = 1,
+  HighRisk = 2,
+  CriticalRisk = 3
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+
+export type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
